192916fb9d203c86fce33edf8d8d4a5c804607f0
eduardofrohlich <eduardofrohlich121@gmail.com>
2023-08-28 12:25:18 -0300

    sum implementation in component soma

--- src/app/soma/soma/soma.component.ts
+++ src/app/soma/soma/soma.component.ts
@@ -7,6 +7,20 @@ import { SomaService } from '../services';
   styleUrls: ['./soma.component.css'],
 })
 export class SomaComponent implements OnInit {
+  private res: number = 0;
+
   constructor(private somaService: SomaService) {}
   ngOnInit(): void {}
+
+  somar(numero1: string, numero2: string): void {
+    let n1: number;
+    let n2: number;
+    n1 = parseFloat(numero1);
+    n2 = parseFloat(numero2);
+    this.res = this.somaService.somar(n1, n2);
+  }
+
+  get resultado(): string {
+    return this.res.toString();
+  }
 }
