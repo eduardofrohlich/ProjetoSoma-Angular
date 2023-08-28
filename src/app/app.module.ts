536829a9449b1bd47bd671f442da45c1d8f9fee8
eduardofrohlich <eduardofrohlich121@gmail.com>
2023-08-28 11:30:25 -0300

    add barrel file index.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SomaModule } from './soma/soma.module';
+import { SomaModule } from './soma';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, SomaModule],
   providers: [],
   bootstrap: [AppComponent],
 })
